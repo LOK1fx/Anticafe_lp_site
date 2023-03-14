b4917a2fe35eef47582b5854b6971d0cf9c85d38
Igor Kravchenko <68440873+LOK1fx@users.noreply.github.com>
2023-03-14 11:07:46 +0900

    Fixed menu button bug

--- src/Anticafe/Map/index.js
+++ src/Anticafe/Map/index.js
@@ -2,5 +2,21 @@ const trigger = document.querySelector('.trigger');
 const nav = document.querySelector('.full-screen-nav');
 const backdrop = document.querySelector('.backdrop');
 
-trigger.addEventListener('click', () => nav.classList.add('open-nav'));
+let isMenuOpened = false
+
+trigger.addEventListener('click', () => on_trigger_clicked());
 backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
+
+function on_trigger_clicked()
+{
+    isMenuOpened = !isMenuOpened
+
+    if (isMenuOpened)
+    {
+        nav.classList.add('open-nav')
+    }
+    else
+    {
+        nav.classList.remove('open-nav')
+    }
+}
